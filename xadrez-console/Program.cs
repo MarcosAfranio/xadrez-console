4824fe5b54abab33dcdf07bf3d1bb53a98535e66
MarcosAfranio <marcosafranio@outlook.com>
2023-01-08 07:56:04 -0300

    Criado a classe PosicaoXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,25 +8,36 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                TabuleiroTabu tab = new TabuleiroTabu(8, 8);
+            #region Tratamento excecoes
+            //try
+            //{
+            //    TabuleiroTabu tab = new TabuleiroTabu(8, 8);
 
-                tab.colocarPeca(new TorreXadrez(tab, CorTabu.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new TorreXadrez(tab, CorTabu.Preta), new Posicao(1, 9));
-                tab.colocarPeca(new ReiXadrez(tab, CorTabu.Preta), new Posicao(0, 2));
-
-
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch(TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            Console.ReadLine();
+            //    tab.colocarPeca(new TorreXadrez(tab, CorTabu.Preta), new Posicao(0, 0));
+            //    tab.colocarPeca(new TorreXadrez(tab, CorTabu.Preta), new Posicao(1, 9));
+            //    tab.colocarPeca(new ReiXadrez(tab, CorTabu.Preta), new Posicao(0, 2));
 
 
+            //    Tela.imprimirTabuleiro(tab);
+            //}
+            //catch(TabuleiroException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //Console.ReadLine();
+            #endregion
+
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+
+            //Console.WriteLine(pos);
+
+            Console.WriteLine(pos.toPosicao());
+
+            Console.ReadKey();
+
+
+                
         }
     }
 }
